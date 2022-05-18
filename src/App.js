c644db01ca07d6641c587b4b4c062416836d002f
Aweroro Hadi <60360809+Aweroro@users.noreply.github.com>
2022-05-18 15:20:41 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import NavBar from './NavBar';
+import Home from './Home';
+
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <div className="content">
+      </div>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
